fcaa4101a22a7ad94c0089b656ccd5cfcf56d287
Chrisha <chrishashre@gmail.com>
2026-06-17 12:56:05 +0545

    Change database connection URL format
    Updated the JDBC URL for the database connection.

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:postgresql://ep-broad-firefly-ao1zov5j-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+    private static final String URL = "jdbc:postgresql:";
     private static final String USER =  "neondb";
     private static final String PASSWORD = "passsword";
 
